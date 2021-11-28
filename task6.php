2d57d10246ab450f274e15e8f37a909bbcd65f8a
Ixtab <workmailixtab@gmail.com>
2021-11-29 00:22:11 +0300

    Lesson-2

---
+++ task6.php
@@ -0,0 +1,16 @@
+<?php
+
+//Задание 6
+
+function power($val, $pow) {
+	if($pow == 1){
+		return $val;
+	}
+	if($pow==0) {
+		return 1;
+	}
+	if($pow < 0){
+		return power( 1/$val, -$pow); 
+		}	
+		return 	$val * power($val, $pow-1);
+}
